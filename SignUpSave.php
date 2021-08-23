c9c1f868ed1dc387453dd44b6093dc34efbc79ed
Sinminbeom <alsqja17@naver.com>
2021-08-23 22:37:47 +0900

    회원가입 중복체크 추가

--- SignUpSave.php
+++ SignUpSave.php
@@ -49,7 +49,12 @@ try
     $fail = array("result"=> false, "message" =>mysqli_error($db));
 
     if($result){
-        echo json_encode($success);
+        $rows = array();
+
+        while($r = mysqli_fetch_assoc($result)) {
+            $rows[] = $r;
+        }
+        echo json_encode($rows);
     }
     else{
         echo json_encode($fail);
